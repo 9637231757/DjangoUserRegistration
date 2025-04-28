06a26a4b5d4cfe6e71c31fa8b20d35c1800ad3d4
9637231757 <er.rohangaidhani@gmail.com>
2025-04-28 11:33:12 +0530

    bug fixed from forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from  .models import forms           
+from .models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['name','photo','email','password','mobile_number','date_of_birth']
+        fields = ['name', 'photo', 'email', 'password', 'mobile_number', 'date_of_birth']
         widgets = {
-            'date_of_birth' : forms.DateInput(attr={'type': 'date'}),
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
             'password': forms.PasswordInput(),
         }
